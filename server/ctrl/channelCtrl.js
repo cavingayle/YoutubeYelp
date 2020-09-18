268c0230763bb6a7829d03b55a4862d463bf1594
Keaton Braithwaite <keeters33@live.com>
2020-09-18 17:17:25 -0600

    changed id

--- server/ctrl/channelCtrl.js
+++ server/ctrl/channelCtrl.js
@@ -1,9 +1,9 @@
 module.exports = {
   addChannel: async (req, res) => {
-    const { id, genre_id } = req.body;
+    const { youtube_id, genre_id } = req.body;
     const db = req.app.get("db");
     db.Channels
-      .add_channel([id, genre_id])
+      .add_channel([youtube_id, genre_id])
       .then((channel) => res.status(200).send(channel))
       .catch((err) => {
         res.status(500).send({
--- server/ctrl/reviewCtrl.js
+++ server/ctrl/reviewCtrl.js
@@ -1,17 +1,17 @@
 module.exports = {
   getReviews: async (req, res) => {
-    const { id } = req.params;
+    const { channel_id } = req.params;
     const db = req.app.get("db");
-    const reviews = await db.Reviews.get_review(id);
+    const reviews = await db.Reviews.get_review(channel_id);
     res.status(200).send(reviews);
   },
 
   addReviews: async (req, res) => {
     const { rating, title, review, user_id } = req.body;
-    const { id } = req.params;
+    const { channel_id } = req.params;
     const db = req.app.get("db");
     db.Reviews
-      .add_review([rating, title, review, user_id, id])
+      .add_review([rating, title, review, channel_id, user_id])
       .then((reviews) => res.status(200).send(reviews))
       .catch((err) => {
         res.status(500).send({
