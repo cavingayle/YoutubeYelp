2b02c1b977031a1fe955020d80679927dd6dd767
Keaton Braithwaite <keeters33@live.com>
2020-09-21 11:20:17 -0600

    started search and searchbar

---
+++ src/components/search/Search.js
@@ -0,0 +1,14 @@
+import React from "react";
+// import { Link } from "react-router-dom";
+// import { getChannelsYT } from "../../redux/reducer";
+
+const Search = (props) => {
+
+    return(
+        <div>
+            {console.log(props)}
+        </div>
+    )
+}
+
+export default Search;
---
+++ src/components/searchbar/Searchbar.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import { getChannelsYT } from "../../redux/reducer";
+import { connect } from "react-redux";
+import Axios from "axios";
+
+const Searchbar = (props) => {
+  const [input, setInput] = useState("");
+
+  const handleInput = (e) => {
+    const { value } = e.target;
+    setInput(value);
+  };
+
+  const handleClearInput = () => {
+    setInput("");
+  };
+
+  const searchChannels = () => {
+    getChannelsYT(input);
+  };
+
+  return (
+    <div>
+      <input onChange={handleInput} value={input} placeholder="Search" />
+      <button onClick={searchChannels}>Search</button>
+    </div>
+  );
+};
