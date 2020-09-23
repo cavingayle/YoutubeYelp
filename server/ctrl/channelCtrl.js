11b18132816a7d02effa8cf76058ec0cf5f253ea
Cavin Gayle <cavingayle@Cavins-MacBook-Pro.local>
2020-09-23 11:50:53 -0400

    fixed channel sql and enpoint

--- server/ctrl/channelCtrl.js
+++ server/ctrl/channelCtrl.js
@@ -1,8 +1,8 @@
 module.exports = {
   addChannel: async (req, res) => {
-    const { youtube_id, genre_id } = req.body;
+    const { youtube_id} = req.body;
     const db = req.app.get("db");
-    db.Channels.add_channel([youtube_id, genre_id])
+    db.Channels.add_channel([youtube_id])
       .then((channel) => res.status(200).send(channel))
       .catch((err) => {
         res.status(500).send({
