cc2798a6fec217ba9d91a1f581d0cb331838927f
Ryan Milne <ryanmilne101@gmail.com>
2020-09-29 14:13:58 -0600

    Some home style work done

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,78 +1,82 @@
-
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import Spinner from "../spinner/Spinner";
 import Searchbar from "../searchbar/Searchbar";
 
-
 function Home(props) {
   const [reviews, setReviews] = useState([]);
-  const [randomChannels, setRandomChannels] = useState([])
-  const [loading, setLoading] = useState(true)
+  const [randomChannels, setRandomChannels] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     axios.get("/api/recent").then((res) => {
-      setReviews(res.data.slice(0,4))
-getRandomChannels()
+      setReviews(res.data.slice(0, 4));
+      getRandomChannels();
     });
-  },[]);
+  }, []);
 
- 
   const getRandomChannels = () => {
-    setLoading(true)
-    axios.get('/api/channels/').then(res => {
-      setRandomChannels(res.data.map((a) => ({sort: Math.random(), value: a}))
-      .sort((a, b) => a.sort - b.sort)
-        .map((a) => a.value).slice(0, 4))
-    setLoading(false)
-    })
-  }
+    setLoading(true);
+    axios.get("/api/channels/").then((res) => {
+      setRandomChannels(
+        res.data
+          .map((a) => ({ sort: Math.random(), value: a }))
+          .sort((a, b) => a.sort - b.sort)
+          .map((a) => a.value)
+          .slice(0, 4)
+      );
+      setLoading(false);
+    });
+  };
 
-
-
-  // filtering the reviews data to remove duplicates 
-  const reviewsFilt = reviews.filter( (rev, ind, self) => 
-  ind === self.findIndex(t =>(
-  t.channel_id === rev.channel_id
-  ))
-  )
-
-
+  // filtering the reviews data to remove duplicates
+  const reviewsFilt = reviews.filter(
+    (rev, ind, self) =>
+      ind === self.findIndex((t) => t.channel_id === rev.channel_id)
+  );
 
   if (loading === true) {
     return (
       <div className="spinner">
-        <Spinner/>
+        <Spinner />
       </div>
     );
   }
-
+  console.log("RandomChan", randomChannels);
+  console.log("reviewFilt", reviewsFilt);
   return (
-    <div>
+    <div className="main-home">
       <header>
-        <div>Youtube Yelp</div>
-        <Searchbar />
+        <h1>YouTube Yelp</h1>
+        <div className="home-searchbar">
+          <Searchbar />
+        </div>
       </header>
-      <div>
-        <div className="genre">Channels To Checkout</div>
-        {randomChannels.map(chan => (
-          <Link to={`/channel/${chan.youtube_id}`}><div >
-            <h2>{chan.title}</h2>
-            <div><img src={chan.image} alt="" width='300'/></div>
-            </div></Link>
-        ))}
-        <span></span>
-        <div className="recentactivity">
-          {reviewsFilt.map((rev,i) => {
+      <div className="home-info">
+        <h1>Random channels to checkout</h1>
+        <div className="random-channel-holder">
+          {randomChannels.map((chan, i) => {
             return (
-              <Link to={`/channel/${rev.youtube_id}`}> <div key={i}>
-                <h2>
-                {rev.channel_title}
-
-                </h2>
-                <div><img src={rev.image} alt={rev.channel_title} width='300'/></div>
-              </div></Link>
+              <Link to={`/channel/${chan.youtube_id}`}>
+                <div key={i} className="random-channel-card">
+                  <h2>{chan.channel_title}</h2>
+                  <img src={chan.image} alt="" width="300" />
+                </div>
+              </Link>
+            );
+          })}
+        </div>
+        <div className="recentActivity">
+          <h1>Recent Activity!</h1>
+          {reviewsFilt.map((rev, i) => {
+            return (
+              <Link to={`/channel/${rev.youtube_id}`}>
+                <div key={i} className="recent-activity-card">
+                  <h2>{rev.channel_title}</h2>
+                  <img src={rev.image} alt={rev.channel_title} width="300" />
+                </div>
+              </Link>
             );
           })}
         </div>
@@ -81,7 +85,4 @@ getRandomChannels()
   );
 }
 
-
-export default Home
-
-
+export default Home;
