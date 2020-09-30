51da5efedcd76b01ad0188b26b54717aff355022
Cavin Gayle <cavingayle@Cavins-MacBook-Pro.local>
2020-09-30 15:59:09 -0400

    updated stars for profile

--- src/components/profile/ProfileReviews.js
+++ src/components/profile/ProfileReviews.js
@@ -6,7 +6,7 @@ function ProfileReviews(props) {
     <div className='pro-review-holder'>
       <p className='pro-review'>Channel: {props.data.channel_title} </p>
       <p className='pro-review'>Title: {props.data.review_title}</p>
-      <p className='pro-review'><Stars rating={props.data.rating} size={10}/></p>
+      <p className='pro-review profile-stars'><Stars rating={props.data.rating} size={10}/></p>
       <p className='pro-review'>Review: {props.data.review}</p>
     </div>
   );
